7e301a9746d2c4791a91ca81f30eb0f700dd8fe4
Ian Parkinson <ianparkinson@gmail.com>
2024-09-10 18:42:41 +0100

    Implementation of MediaServer allowing use of USB drives attached to the CXNv2.
    This initial implementation ignores a number of limitations of the CXNv2 ContentDirectory (see the comments) - in particular, it does not tolerate putting the streamer into standby.
    
    Still, it acts as a base for appropriate workarounds to be added.

--- vibin/mediaservers/__init__.py
+++ vibin/mediaservers/__init__.py
@@ -2,6 +2,7 @@ from .mediaserver import MediaServer
 
 # Each MediaServer implementation should be imported here.
 from .asset import Asset
+from .cxnv2 import CXNv2
 
 # Map UPnP device models to Media Server implementations. This is not required
 # if the UPnP model name is the same as the implementation name.
