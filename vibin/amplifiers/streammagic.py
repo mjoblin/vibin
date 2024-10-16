6fd4c3d7633d28daad095bdf71a819d3871c06cb
Ian Parkinson <ianparkinson@gmail.com>
2024-10-16 16:52:33 +0100

    Use `volume_step` instead of `volume_percent`
    Fixes a bug where vibinui causes the volume to slowly decrease until it hits a multiple of 10%.
    
    The CXNv2 itself has 30 steps of volume. It appears to round values down when applying a percentage volume, and again when reporting the new volume back via the `volume_percent` field.
    
    Hence, setting the volume to, say, 95% causes the streamer to set its volume to floor(30*95%) = 28, which is reported back as floor(28/30) = 93%.
    
    This, in turn, triggers the UI's volume change detection logic in `KeyboardShortcutsManager`, which bounces the new volume value of 93% back to the streamer.
    
    But the streamer then interprets this requested value of 93% as floor(30*93%) = 27, which gets reported back as floor(27/30) = 90%. Once again, the UI bounces that new volume of 90% back to the streamer, but because floor(30*90%) is still 27, this is now stable.
    
    This server-side fix rounds to the nearest available step, which means that the reported fractional volume can still differ from the requested fractional volume. Under some circumstances the client still bounces that changed volume back, but it remains stable.

--- vibin/amplifiers/streammagic.py
+++ vibin/amplifiers/streammagic.py
@@ -61,20 +61,14 @@ class StreamMagic(Amplifier):
         self._on_update = on_update
 
         self._device_hostname = urlparse(device.location).hostname
-        self._device_state = AmplifierState(
-            name=self._device.friendly_name,
-            actions=[],
-            power=None,
-            mute=None,
-            volume=None,
-            sources=None,
-        )
+        self._state_data = None
+        self._max_volume_step = None
 
         self._websocket_thread = WebsocketThread(
             uri=f"ws://{self._device_hostname}:80/smoip",
             friendly_name=self._device.friendly_name,
             on_connect=self._initialize_websocket,
-            on_data=self._handle_state_message,
+            on_data=self._handle_websocket_message,
             on_disconnect=self._on_disconnect,
         )
         self._websocket_thread.start()
@@ -97,7 +91,7 @@ class StreamMagic(Amplifier):
     @property
     def device_state(self) -> AmplifierState:
         """System state for the Amplifier."""
-        return self._device_state
+        return self._compute_amplifier_state()
 
     @property
     def device_udn(self) -> str:
@@ -120,12 +114,12 @@ class StreamMagic(Amplifier):
 
     @property
     def actions(self) -> list[AmplifierAction]:
-        return self._device_state.actions
+        return self.device_state.actions
 
     @property
     def power(self) -> PowerState | None:
         """Power state."""
-        return self._device_state.power
+        return self.device_state.power
 
     @power.setter
     def power(self, state: PowerState) -> None:
@@ -144,46 +138,48 @@ class StreamMagic(Amplifier):
     @property
     def volume(self) -> float | None:
         """Current volume (0-1)."""
-        return self._device_state.volume
+        return self.device_state.volume
 
     @volume.setter
     def volume(self, volume: float) -> None:
         """Set the volume (0-1)."""
-        if "volume" in self._device_state.actions:
-            self._send_state_request("volume_percent", str(round(volume * 100)))
+        if "volume" in self.device_state.actions and self._max_volume_step:
+            self._send_state_request(
+                "volume_step", str(round(volume * self._max_volume_step))
+            )
 
     def volume_up(self) -> None:
         """Increase the volume by one unit."""
-        if "volume_up_down" in self._device_state.actions:
+        if "volume_up_down" in self.device_state.actions:
             self._send_state_request("volume_step_change", "1")
 
     def volume_down(self) -> None:
         """Decrease the volume by one unit."""
-        if "volume_up_down" in self._device_state.actions:
+        if "volume_up_down" in self.device_state.actions:
             self._send_state_request("volume_step_change", "-1")
 
     @property
     def mute(self) -> MuteState | None:
         """Mute state."""
-        return self._device_state.mute
+        return self.device_state.mute
 
     @mute.setter
     def mute(self, state: MuteState) -> None:
         """Set the mute state."""
-        if "mute" in self._device_state.actions:
+        if "mute" in self.device_state.actions:
             self._send_state_request("mute", "true" if state == "on" else "false")
 
     def mute_toggle(self) -> None:
         """Toggle the mute state."""
-        if "mute" in self._device_state.actions:
+        if "mute" in self.device_state.actions:
             self._send_state_request(
-                "mute", "false" if self._device_state.mute == "on" else "true"
+                "mute", "false" if self.device_state.mute == "on" else "true"
             )
 
     @property
     def audio_sources(self) -> AudioSources | None:
         """Not supported."""
-        return self._device_state.sources
+        return self.device_state.sources
 
     @property
     def audio_source(self) -> AudioSource | None:
@@ -216,54 +212,83 @@ class StreamMagic(Amplifier):
 
     async def _initialize_websocket(self, websocket: WebSocketClientProtocol):
         """On connection to the WebSocket, subscribe to StreamMagic events."""
+        await websocket.send('{ "path": "/zone/state/spec", "params": { "update": 1 }}')
         await websocket.send('{ "path": "/zone/state", "params": { "update": 1 }}')
         if self._on_connect:
             self._on_connect()
 
-    def _handle_state_message(self, message: Data):
+    def _handle_websocket_message(self, message: Data):
         try:
             parsed = json.loads(message)
-        except (KeyError, json.decoder.JSONDecodeError):
+        except json.decoder.JSONDecodeError:
             return
 
-        if parsed["path"] != "/zone/state" or parsed["type"] != "update":
+        if "path" not in parsed:
             pass
 
-        data = parsed["params"]["data"]
+        match parsed["path"]:
+            case "/zone/state":
+                self._state_data = parsed["params"]["data"]
+                self._on_update("System", self.device_state)
+            case "/zone/state/spec":
+                try:
+                    # volume_step is only present in pre-amp mode
+                    self._max_volume_step = parsed["params"]["data"]["volume_step"][
+                        "maximum"
+                    ]
+                    self._on_update("System", self.device_state)
+                except KeyError:
+                    pass
+            case _:
+                pass
 
-        # Convert the update message to an AmplifierState.
-        # Report the current power status, so that the Vibin UI can grey out
-        # controls if the streamer isn't currently available, even though this
-        # implementation doesn't support controlling the power.
-        if data["pre_amp_mode"]:
-            self._device_state = AmplifierState(
+    def _compute_amplifier_state(self) -> AmplifierState:
+        """Converts the state messages to an AmplifierState.
+
+        Reports the current power status, so that the Vibin UI can grey out
+        controls if the streamer isn't currently available, even though this
+        implementation doesn't support controlling the power.
+        """
+        if self._state_data and self._state_data["pre_amp_mode"]:
+            return AmplifierState(
                 name=self._device.friendly_name,
                 actions=["volume", "mute", "volume_up_down"],
-                power="on" if data["power"] else "off",
-                mute="on" if data["mute"] else "off",
-                volume=data["volume_percent"] / 100,
+                power="on" if self._state_data["power"] else "off",
+                mute="on" if self._state_data["mute"] else "off",
+                volume=(
+                    self._state_data["volume_step"] / self._max_volume_step
+                    if self._max_volume_step
+                    else None
+                ),
                 sources=None,
             )
-        elif data["cbus"] in ["amplifier", "receiver"]:
-            self._device_state = AmplifierState(
+        elif self._state_data and self._state_data["cbus"] in ["amplifier", "receiver"]:
+            return AmplifierState(
                 name=self._device.friendly_name,
                 actions=["volume_up_down"],
-                power="on" if data["power"] else "off",
+                power="on" if self._state_data["power"] else "off",
+                mute=None,
+                volume=None,
+                sources=None,
+            )
+        elif self._state_data:
+            return AmplifierState(
+                name=self._device.friendly_name,
+                actions=[],
+                power="on" if self._state_data["power"] else "off",
                 mute=None,
                 volume=None,
                 sources=None,
             )
         else:
-            self._device_state = AmplifierState(
+            return AmplifierState(
                 name=self._device.friendly_name,
                 actions=[],
-                power="on" if data["power"] else "off",
+                power=None,
                 mute=None,
                 volume=None,
                 sources=None,
             )
-
-        self._on_update("System", self._device_state)
 
     def _send_state_request(self, param: str, value: str):
         requests.get(f"http://{self._device_hostname}/smoip/zone/state?{param}={value}")
