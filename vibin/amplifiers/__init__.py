1d3787c644beffb428f3f6f81c3d4dc8963774b5
Michael Joblin <mjoblin@users.noreply.github.com>
2025-12-21 17:17:27 -0800

    Update streamer and amplifier lists

--- vibin/amplifiers/__init__.py
+++ vibin/amplifiers/__init__.py
@@ -6,12 +6,27 @@ from .streammagic import StreamMagic
 
 # Map UPnP device models to Amplifier implementations. This is not required if
 # the UPnP model name is the same as the implementation name.
+#
+# Note: Not all of these have been confirmed.
 model_to_amplifier = {
+    # Hegel
     "H95": Hegel,
     "H120": Hegel,
     "H190": Hegel,
     "H390": Hegel,
     "H590": Hegel,
     "H600": Hegel,
+    # Cambridge Audio StreamMagic (streamers with preamp mode)
+    "851N": StreamMagic,
+    "AXN10": StreamMagic,
+    "CXN100": StreamMagic,
     "CXNv2": StreamMagic,
+    "Edge NQ": StreamMagic,
+    "EXN100": StreamMagic,
+    "MXN10": StreamMagic,
+    # Cambridge Audio StreamMagic (streaming amplifiers)
+    "Evo 75": StreamMagic,
+    "Evo 150": StreamMagic,
+    "Evo 150 SE": StreamMagic,
+    "Evo ONE": StreamMagic,
 }
